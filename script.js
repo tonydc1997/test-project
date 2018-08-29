84cc12ebf54bd3dae8a0af3b1786bc0c7ccb8fc9
Anthony Curtis <tonydc1997@gmail.com>
2018-08-29 11:33:50 -0700

    Finish basic search functionality

--- script.js
+++ script.js
@@ -11,4 +11,5 @@ const googleSearch = (searchInput) => {
   const matches = googleDatabase.filter(website => {
     return website.includes(searchInput);
   })
+  return matches.length > 3 ? matches.slice(0, 3) : matches;
 }
