57c16cf0a2528689def379e4c49e63d3ed97f9a1
Anthony Curtis <tonydc1997@gmail.com>
2018-08-29 12:21:43 -0700

    Group relevant tests together

--- script.test.js
+++ script.test.js
@@ -7,20 +7,23 @@ dbMock = [
   'bossbaby.com'
 ];
 
-it('is Testing... Testing... 1.. 2.. 3..', () => {
-  expect('hello').toBe('hello');
-})
-
-it('is testing googleSearch', () => {
-  expect(googleSearch('placeholder text', dbMock)).toEqual([]);
-  expect(googleSearch('dog', dbMock)).toEqual(['dog.com']);
-})
-
-it('works with undefined and null input', () => {
-  expect(googleSearch(undefined, dbMock)).toEqual([]);
-  expect(googleSearch(null, dbMock)).toEqual([]);
-})
-
-it('does not return more than 3 matches', () => {
-  expect(googleSearch('.com', dbMock).length).toEqual(3);
+describe('googleSearch', () => {
+  it('is Testing... Testing... 1.. 2.. 3..', () => {
+    expect('hello').toBe('hello');
+  })
+  
+  it('is testing googleSearch', () => {
+    expect(googleSearch('placeholder text', dbMock)).toEqual([]);
+    expect(googleSearch('dog', dbMock)).toEqual(['dog.com']);
+  })
+  
+  it('works with undefined and null input', () => {
+    expect(googleSearch(undefined, dbMock)).toEqual([]);
+    expect(googleSearch(null, dbMock)).toEqual([]);
+  })
+  
+  it('does not return more than 3 matches', () => {
+    expect(googleSearch('.com', dbMock).length).toEqual(3);
+  })
+  
 })
