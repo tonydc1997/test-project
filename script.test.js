76d1d00029645c3bfa845c055b071b78edf2cc2a
Anthony Curtis <tonydc1997@gmail.com>
2018-08-29 11:55:01 -0700

    Create mock db

--- script.test.js
+++ script.test.js
@@ -1,6 +1,11 @@
 const googleSearch = require('./script');
 
-
+dbMock = [
+  'dog.com',
+  'reesespuff.com',
+  'disney.com',
+  'bossbaby.com'
+];
 
 it('Testing... Testing... 1.. 2.. 3..', () => {
   googleSearch('dummy-place-holder', dbMock);
