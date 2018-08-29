b4db36e72d986e0e4f59c184fcc1c3198e891293
Anthony Curtis <tonydc1997@gmail.com>
2018-08-29 12:20:00 -0700

    Test matches functionality

--- script.test.js
+++ script.test.js
@@ -20,3 +20,7 @@ it('works with undefined and null input', () => {
   expect(googleSearch(undefined, dbMock)).toEqual([]);
   expect(googleSearch(null, dbMock)).toEqual([]);
 })
+
+it('does not return more than 3 matches', () => {
+  expect(googleSearch('.com', dbMock).length).toEqual(3);
+})
