48630c3754b5d037356f4241cff0a227c01c5cac
Anthony Curtis <tonydc1997@gmail.com>
2018-08-29 11:53:10 -0700

    Make search function extensible

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ const googleDatabase = [
   'myfavouritecats.com'
 ];
 
-const googleSearch = (searchInput) => {
-  const matches = googleDatabase.filter(website => {
+const googleSearch = (searchInput, db) => {
+  const matches = db.filter(website => {
     return website.includes(searchInput);
   })
   return matches.length > 3 ? matches.slice(0, 3) : matches;
