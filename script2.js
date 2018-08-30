7a19cf5b47d38b3821ef236b2bf993dc6de6d5c0
Anthony Curtis <tonydc1997@gmail.com>
2018-08-30 09:06:12 -0700

    Create async function

--- script2.js
+++ script2.js
@@ -1,7 +1,19 @@
 const fetch = require('node-fetch');
 
-const getPeople = fetch => {
+const getPeoplePromise = fetch => {
   return fetch('https://swapi.co/api/people')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      return {
+        count: data.count,
+        results: data.results
+      }
+    })
+}
+
+const getPeople =  async (fetch) => {
+  const getResponse = await fetch('https://swapi.co/api/people')
     .then(response => response.json())
     .then(data => {
       console.log(data);
