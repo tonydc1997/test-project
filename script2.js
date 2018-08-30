6eee5326dae692941459691bed2e25a04f5d0328
Anthony Curtis <tonydc1997@gmail.com>
2018-08-30 09:01:50 -0700

    Finish and test functionality

--- script2.js
+++ script2.js
@@ -4,7 +4,11 @@ const getPeople = fetch => {
   return fetch('https://swapi.co/api/people')
     .then(response => response.json())
     .then(data => {
-      
+      return {
+        count: data.count,
+        results: data.results
+      }
     })
 }
 
+console.log(getPeople(fetch));
