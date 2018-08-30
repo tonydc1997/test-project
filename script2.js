510ffbd2076c943bd9cdce658c07054aed325737
Anthony Curtis <tonydc1997@gmail.com>
2018-08-30 09:03:55 -0700

    Fix code placement for testing purposes

--- script2.js
+++ script2.js
@@ -4,6 +4,7 @@ const getPeople = fetch => {
   return fetch('https://swapi.co/api/people')
     .then(response => response.json())
     .then(data => {
+      console.log(data);
       return {
         count: data.count,
         results: data.results
@@ -11,4 +12,4 @@ const getPeople = fetch => {
     })
 }
 
-console.log(getPeople(fetch));
+getPeople(fetch);
