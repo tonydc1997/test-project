0861814d86e3f8a2823ec3f7435773ffd1e384e2
Anthony Curtis <tonydc1997@gmail.com>
2018-08-30 08:59:15 -0700

    Build onto function with appropriate fetch call and response.json

--- script2.js
+++ script2.js
@@ -1,6 +1,10 @@
 const fetch = require('node-fetch');
 
 const getPeople = fetch => {
-  return fetch();
+  return fetch('https://swapi.co/api/people')
+    .then(response => response.json())
+    .then(data => {
+      
+    })
 }
 
