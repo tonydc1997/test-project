925043011fd63824a1a46164dc366107fa9b3545
Anthony Curtis <tonydc1997@gmail.com>
2018-08-30 09:22:33 -0700

    Write test for async await

--- script2.test.js
+++ script2.test.js
@@ -2,5 +2,7 @@ const fetch = require('node-fetch');
 const swapi = require('./script2');
 
 it('calls swapi to get people', () => {
-  
+  swapi.getPeople(fetch).then(data => {
+    expect(data.count).toEqual(87);
+  })
 })
