3d70f5e49a6e472b27289fc0a4541bcdc5de3936
Anthony Curtis <tonydc1997@gmail.com>
2018-08-30 09:30:00 -0700

    Modify test to wait until pending state is done / complete

--- script2.test.js
+++ script2.test.js
@@ -1,10 +1,11 @@
 const fetch = require('node-fetch');
 const swapi = require('./script2');
 
-it('calls swapi to get people', () => {
+it('calls swapi to get people', (done) => {
   expect.assertions(1);
   swapi.getPeople(fetch).then(data => {
     expect(data.count).toEqual(87);
+    done();
   })
 })
 
